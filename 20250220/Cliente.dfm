c30fef8643dc11d4575838fbca85567f1ec9f198
andremuriae@yahoo.com.br <andrerorizfernandes>
2025-02-27 20:03:52 -0300

    Ajustes do dia 27/02/2025

--- 20250220/Cliente.dfm
+++ 20250220/Cliente.dfm
@@ -4,7 +4,7 @@ object frmCliente: TfrmCliente
   BorderStyle = bsSingle
   Caption = 'Cliente'
   ClientHeight = 360
-  ClientWidth = 624
+  ClientWidth = 667
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -12,69 +12,98 @@ object frmCliente: TfrmCliente
   Font.Name = 'Segoe UI'
   Font.Style = []
   Position = poScreenCenter
+  OnActivate = FormActivate
   TextHeight = 15
-  object pnlPrincipal: TPanel
+  object memResultado: TMemo
+    Left = 0
+    Top = 68
+    Width = 667
+    Height = 261
+    Align = alClient
+    Enabled = False
+    TabOrder = 0
+    ExplicitHeight = 252
+  end
+  object gbxParametrosEntrada: TGroupBox
     Left = 0
     Top = 0
-    Width = 624
-    Height = 329
+    Width = 667
+    Height = 68
     Align = alTop
-    TabOrder = 0
-    object memResultado: TMemo
-      Left = 1
-      Top = 24
-      Width = 622
-      Height = 304
-      Align = alBottom
-      Enabled = False
-      TabOrder = 2
+    Caption = ' Par'#226'metros de entrada '
+    TabOrder = 1
+    ExplicitWidth = 624
+    object lblValor: TLabel
+      Left = 173
+      Top = 18
+      Width = 26
+      Height = 15
+      Caption = 'Valor'
+    end
+    object lblEstrutura: TLabel
+      Left = 8
+      Top = 18
+      Width = 47
+      Height = 15
+      Caption = 'Estrutura'
     end
     object edtQuantidade: TEdit
-      Left = 1
-      Top = 1
+      Left = 173
+      Top = 35
       Width = 121
       Height = 23
-      Align = alLeft
+      Hint = 'Informe a quantidade'
       MaxLength = 7
       NumbersOnly = True
-      TabOrder = 0
-      ExplicitLeft = 112
-      ExplicitTop = 16
+      ParentShowHint = False
+      ShowHint = True
+      TabOrder = 1
+      OnKeyPress = edtQuantidadeKeyPress
     end
     object cboEstrutura: TComboBox
-      Left = 122
-      Top = 1
+      Left = 8
+      Top = 35
       Width = 159
       Height = 23
-      Align = alLeft
+      Hint = 'Selecione a estrutura'
       Style = csDropDownList
-      ItemIndex = 0
-      TabOrder = 1
-      Text = 'While'
-      Items.Strings = (
-        'While'
-        'Repeat'
-        'For')
+      ParentShowHint = False
+      ShowHint = True
+      TabOrder = 0
     end
   end
-  object btnTeste: TButton
-    Left = 528
+  object pnlRodape: TPanel
+    Left = 0
     Top = 329
-    Width = 96
+    Width = 667
     Height = 31
-    Align = alRight
-    Caption = 'Teste'
-    TabOrder = 1
-    OnClick = btnTesteClick
-  end
-  object btnPovoar: TButton
-    Left = 432
-    Top = 329
-    Width = 96
-    Height = 31
-    Align = alRight
-    Caption = '&Povoar'
+    Align = alBottom
     TabOrder = 2
-    OnClick = btnPovoarClick
+    object btnPovoar: TButton
+      Left = 474
+      Top = 1
+      Width = 96
+      Height = 29
+      Align = alRight
+      Caption = '&Povoar'
+      TabOrder = 0
+      OnClick = btnPovoarClick
+      ExplicitLeft = 426
+      ExplicitTop = 10
+      ExplicitHeight = 31
+    end
+    object btnLimpar: TButton
+      Left = 570
+      Top = 1
+      Width = 96
+      Height = 29
+      Align = alRight
+      Caption = 'Limpar'
+      TabOrder = 1
+      OnClick = btnLimparClick
+      ExplicitLeft = 528
+      ExplicitTop = 10
+      ExplicitHeight = 31
+    end
   end
 end
--- 20250220/Cliente.pas
+++ 20250220/Cliente.pas
@@ -8,19 +8,30 @@ uses
 
 type
   TfrmCliente = class(TForm)
-    pnlPrincipal: TPanel;
     memResultado: TMemo;
-    btnTeste: TButton;
+    gbxParametrosEntrada: TGroupBox;
     edtQuantidade: TEdit;
-    btnPovoar: TButton;
     cboEstrutura: TComboBox;
-    procedure btnTesteClick(Sender: TObject);
+    lblValor: TLabel;
+    lblEstrutura: TLabel;
+    pnlRodape: TPanel;
+    btnPovoar: TButton;
+    btnLimpar: TButton;
+    procedure btnLimparClick(Sender: TObject);
     procedure btnPovoarClick(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure edtQuantidadeKeyPress(Sender: TObject; var Key: Char);
   private
     procedure PovoarLista;
     procedure PreencherListaUtilizandoWhile(pQuantidade: Integer);
     procedure PreencherListaUtilizandoRepeat(pQuantidade: Integer);
     procedure PreencherListaUtilizandoFor(pQuantidade: Integer);
+    procedure PreencherWhileComBreak(pQuantidade: Integer);
+    procedure PreencherForComContinue(pQuantidade: Integer);
+    procedure PreencherCampoTipoEstrutura;
+    procedure LimparTela;
+    procedure ValidarDados;
+    function ExibirUltimoElemento(pValores: TStrings): string;
     { Private declarations }
   public
     { Public declarations }
@@ -38,15 +49,38 @@ const
 
 procedure TfrmCliente.btnPovoarClick(Sender: TObject);
 begin
+  ValidarDados;
   PovoarLista;
 end;
 
-procedure TfrmCliente.btnTesteClick(Sender: TObject);
+procedure TfrmCliente.edtQuantidadeKeyPress(Sender: TObject; var Key: Char);
 begin
-  const TURMA = 'ADS';
-  var lTeste := 'ola mundo';
-  memResultado.Lines.Add(lTeste);
-  IntToStr(30);
+  if Key = #13 then
+    btnPovoar.Click;
+end;
+
+function TfrmCliente.ExibirUltimoElemento(pValores: TStrings): string;
+begin
+  var lQuantidadeLinhas := pValores.Count;
+  Result := pValores[lQuantidadeLinhas - 1];
+end;
+
+procedure TfrmCliente.btnLimparClick(Sender: TObject);
+begin
+  LimparTela;
+end;
+
+procedure TfrmCliente.FormActivate(Sender: TObject);
+begin
+  PreencherCampoTipoEstrutura;
+end;
+
+procedure TfrmCliente.LimparTela;
+begin
+  memResultado.Clear;
+  edtQuantidade.Clear;
+  cboEstrutura.ItemIndex := -1;
+  edtQuantidade.SetFocus;
 end;
 
 procedure TfrmCliente.PreencherListaUtilizandoWhile(pQuantidade: Integer);
@@ -60,6 +94,41 @@ begin
   end;
 end;
 
+procedure TfrmCliente.PreencherWhileComBreak(pQuantidade: Integer);
+begin
+  var I := 1;
+  memResultado.Lines.Clear;
+  while (I <= pQuantidade) do
+  begin
+    memResultado.Lines.Add(Format(MENSAGEM_DA_LISTA, [I.ToString]));
+
+    if (I = 5) then
+      Break;
+
+    Inc(I);
+  end;
+end;
+
+procedure TfrmCliente.ValidarDados;
+const
+  MENSAGEM_ESTRUTURA = 'Selecione a estrutura';
+  MENSAGEM_QUANTIDADE = 'Informe a quantidade';
+begin
+  if (cboEstrutura.ItemIndex = -1) then
+  begin
+    ShowMessage(MENSAGEM_ESTRUTURA);
+    cboEstrutura.SetFocus;
+    Abort;
+  end;
+
+  if (edtQuantidade.Text = EmptyStr) then
+  begin
+    ShowMessage(MENSAGEM_QUANTIDADE);
+    edtQuantidade.SetFocus;
+    Abort;
+  end;
+end;
+
 procedure TfrmCliente.PreencherListaUtilizandoRepeat(pQuantidade: Integer);
 begin
   var I := 1;
@@ -68,6 +137,29 @@ begin
     memResultado.Lines.Add(Format(MENSAGEM_DA_LISTA, [I.ToString]));
     I := I + 1;
   until (I > pQuantidade);
+end;
+
+procedure TfrmCliente.PreencherCampoTipoEstrutura;
+begin
+  cboEstrutura.Items.Clear;
+  cboEstrutura.Items.Add('While');
+  cboEstrutura.Items.Add('Repeat');
+  cboEstrutura.Items.Add('For');
+  cboEstrutura.Items.Add('While com break');
+  cboEstrutura.Items.Add('For com continue');
+  cboEstrutura.SetFocus;
+end;
+
+procedure TfrmCliente.PreencherForComContinue(pQuantidade: Integer);
+begin
+  memResultado.Lines.Clear;
+  for var I := 1 to pQuantidade do
+  begin
+    if (I mod 2 = 0) then
+      Continue;
+
+    memResultado.Lines.Add(Format(MENSAGEM_DA_LISTA, [I.ToString]));
+  end;
 end;
 
 procedure TfrmCliente.PreencherListaUtilizandoFor(pQuantidade: Integer);
@@ -88,8 +180,12 @@ begin
     0: PreencherListaUtilizandoWhile(lQuantidade);
     1: PreencherListaUtilizandoRepeat(lQuantidade);
     2: PreencherListaUtilizandoFor(lQuantidade);
+    3: PreencherWhileComBreak(lQuantidade);
+    4: PreencherForComContinue(lQuantidade);
   end;
 
+  var lUltimoElemento := ExibirUltimoElemento(memResultado.Lines);
+  ShowMessage(lUltimoElemento);
   edtQuantidade.SetFocus;
 end;
 end.
